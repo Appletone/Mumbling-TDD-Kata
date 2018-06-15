2315d80fecf91499d58ceb2cc8ebce230b4c6cd8
Louis Chang <Louis.Chang@xuenn.com>
2018-06-15 15:37:39 +0800

    refactor(ViewController): Remove useless code

--- Mumbling-TDD-Kata/ViewController.swift
+++ Mumbling-TDD-Kata/ViewController.swift
@@ -10,16 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     func accum(_ letters:String) -> String {
         var ret = letters.enumerated().map { String(repeating: $1, count: $0 + 1).capitalized + "-" }.reduce("", +)
         ret.removeLast()
