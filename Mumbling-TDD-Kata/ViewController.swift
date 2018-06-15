638589d3baf8a33e2b99244fc6552b3608da0e54
Louis Chang <Louis.Chang@xuenn.com>
2018-06-15 10:22:40 +0800

    Initial Commit

---
+++ Mumbling-TDD-Kata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mumbling-TDD-Kata
+//
+//  Created by 07423.louis.chang on 2018/6/15.
+//  Copyright © 2018年 Louis Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
